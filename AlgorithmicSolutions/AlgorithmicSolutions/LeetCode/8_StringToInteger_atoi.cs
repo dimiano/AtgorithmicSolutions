96e08ac9ccb2027e56567b25728c2fcefac28794
dtuchyk <dmytro.tuchyk@infor.com>
2018-07-18 16:41:15 +0200

    String to Integer (atoi)

--- AlgorithmicSolutions/AlgorithmicSolutions/LeetCode/8_StringToInteger_atoi.cs
+++ AlgorithmicSolutions/AlgorithmicSolutions/LeetCode/8_StringToInteger_atoi.cs
@@ -71,19 +71,144 @@ namespace AlgorithmicSolutions.LeetCode
 			TestCase3();
 			TestCase4();
 			TestCase5();
+			TestCase6();
+			TestCase7();
+			TestCase8();
+			TestCase9();
+			TestCase10();
+			TestCase11();
+			TestCase12();
+			TestCase13();
+			TestCase14();
+			TestCase15();
+			TestCase16();
+			TestCase17();
 		}
 
 		private static int GetResult(string str)
 		{
+			if (str.Length == 0)
+			{
+				return 0;
+			}
 
-			return 0;
+			var num = new int[10]; // int.MaxValue length = 10 (2 147 483 647)
+
+			for (int i = 0; i < num.Length; i++)
+			{
+				num[i] = -1;
+			}
+
+			var negative = false;
+			var digitPresent = false;
+			var plusPresent = false;
+			var leadZero = false;
+			var n = 0;
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (char.IsDigit(str[i]))
+				{
+					if (str[i] == '0' && !digitPresent) // ignore leading zero
+					{
+						leadZero = true;
+						continue;
+					}
+					else if (n >= num.Length) // overflow int32
+					{
+						return negative ? int.MinValue : int.MaxValue;
+					}
+
+					num[n] = GetInt(str[i]);
+					n++;
+					digitPresent = true;
+				}
+				else if ((str[i] == ' ') && !digitPresent && !plusPresent && !negative && !leadZero)
+				{
+					continue;
+				}
+				else
+				{
+					if (str[i] == '-' && !digitPresent && !plusPresent && !leadZero && !negative)
+					{
+						negative = true;
+					}
+					else if (str[i] == '+' && !plusPresent && !negative && !leadZero)
+					{
+						plusPresent = true;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			var result = 0;
+			var factor = 1;
+
+			for (int i = num.Length - 1; i >= 0; i--)
+			{
+				if (num[i] == -1)
+				{
+					continue;
+				}
+
+				if (negative)
+				{
+					result -= num[i] * factor;
+				}
+				else
+				{
+					result += num[i] * factor;
+				}
+
+				factor *= 10;
+			}
+
+			if (n == 10 && num[0] > 2)
+			{
+				result = negative ? int.MinValue : int.MaxValue;
+			}
+			else
+			{
+				if (result < 0 && !negative) // int32 overflow +
+				{
+					result = int.MaxValue;
+				}
+				else if (result > 0 && negative) // int32 overflow -
+				{
+					result = int.MinValue;
+				}
+			}
+
+			return result;
+		}
+
+		private static int GetInt(char ch)
+		{
+			switch (ch)
+			{
+				case '0': return 0;
+				case '1': return 1;
+				case '2': return 2;
+				case '3': return 3;
+				case '4': return 4;
+				case '5': return 5;
+				case '6': return 6;
+				case '7': return 7;
+				case '8': return 8;
+				case '9': return 9;
+			}
+
+			throw new ArgumentException(nameof(ch));
 		}
 
 
 		private static void TestCase1()
 		{
 			var input = "24";
-			var expect = 42;
+			var expect = 24;
 
 			var result = GetResult(input);
 
@@ -99,6 +224,7 @@ namespace AlgorithmicSolutions.LeetCode
 
 			WriteResult(input, expect, result);
 		}
+
 		private static void TestCase3()
 		{
 			var input = "4193 with words";
@@ -108,6 +234,7 @@ namespace AlgorithmicSolutions.LeetCode
 
 			WriteResult(input, expect, result);
 		}
+
 		private static void TestCase4()
 		{
 			var input = "words and 987";
@@ -117,10 +244,131 @@ namespace AlgorithmicSolutions.LeetCode
 
 			WriteResult(input, expect, result);
 		}
+
 		private static void TestCase5()
 		{
 			var input = "-91283472332";
 			var expect = int.MinValue;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase6()
+		{
+			var input = "+1";
+			var expect = 1;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase7()
+		{
+			var input = "  0000000000012345678";
+			var expect = 12345678;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase8()
+		{
+			var input = "+-2";
+			var expect = 0;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase9()
+		{
+			var input = "-+1";
+			var expect = 0;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase10()
+		{
+			var input = "   +0 123";
+			var expect = 0;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase11()
+		{
+			var input = "2147483648";
+			var expect = 2147483647;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase12()
+		{
+			var input = "-2147483649";
+			var expect = -2147483648;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase13()
+		{
+			var input = "-6147483648";
+			var expect = -2147483648;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase14()
+		{
+			var input = "20000000000000000000";
+			var expect = 2147483647; // int.Max
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase15()
+		{
+			var input = "0-1";
+			var expect = 0;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase16()
+		{
+			var input = "0  123";
+			var expect = 0;
+
+			var result = GetResult(input);
+
+			WriteResult(input, expect, result);
+		}
+
+		private static void TestCase17()
+		{
+			var input = " --2";
+			var expect = 0;
 
 			var result = GetResult(input);
 
